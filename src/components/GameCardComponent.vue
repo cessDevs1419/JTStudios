bf693eb60f495c530bfb52baafbde90c49226584
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-03-08 17:40:01 +0800

    updating game card template

--- src/components/GameCardComponent.vue
+++ src/components/GameCardComponent.vue
@@ -6,7 +6,7 @@
         props: {
             data: {
                 type: Object
-            }
+            },
         },
         methods: {
             iconSetter(icon) {
@@ -18,16 +18,35 @@
 </script>
 
 <template>
-    <div class="card dark-grey-bg" >
-        <img :src="data.images" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h6 class="card-title primary-font">{{data.title}}</h6>
-            <div class="icon-container m-auto mb-3 d-flex justify-content-start align-items-center">
-                <i v-for="icons in data.platforms" :class="iconSetter(icons.type)" class="bi tertiary-font me-2 fs-6"></i>
+    <div class="card dark-grey-bg m-auto" >
+        <div class="card-header w-100 position-relative p-0 m-0">
+            <div class="status-container position-absolute mt-2 ms-2 primary-bg border border-2 rounded-3 light-grey-border px-2 py-1">
+                <h6 class="m-0 accent-color fw-light" v-if="data.status">{{data.status}}</h6>
+            </div>
+            <img :src="data.images" class="card-img-top" alt="...">
+        </div>
+        <div class="card-body p-3">
+            <h6 class="card-title primary-font m-0 mb-1">{{data.title}}</h6>
+            <div class="icon-container m-auto d-flex justify-content-start align-items-center">
+                <i v-for="(icons, index) in data.platforms" :key="index" :class="iconSetter(icons.type)" class="bi tertiary-font me-2 fs-6"></i>
             </div>
         </div>
-        <div class="card-footer">
-        
+        <div class="card-footer light-grey-border justify-content-between d-flex align-items-center p-2">
+            <div class="action-btn">
+                <button class="btn-action btn btn-danger border-0 light-grey-bg me-2 p-0">
+                    <i class="bi bi-pencil-square primary-font fs-5 m-0 "></i>
+                </button>
+                <button class="btn-action btn btn-danger border-0 light-grey-bg me-2  p-0">
+                    <i class="bi bi-trash primary-font fs-5 m-0 "></i>
+                </button>
+                <button class="btn-action btn btn-danger border-0 light-grey-bg me-2  p-0">
+                    <i class="bi bi-eye primary-font fs-5 m-0 "></i>
+                </button>
+            </div>
+            <button class="btn-visit btn btn-danger d-flex align-items-center border-0 px-2 py-1 dark-grey-subtle-bg light-grey-font">
+                <h6 class="m-0 me-1 fw-light">Visit Page</h6>
+                <i class="bi bi-arrow-right fs-6 m-0 "></i>
+            </button>
         </div>
     </div>
 </template>
@@ -36,11 +55,22 @@
     .card{
         max-width: 300px;
     }
-
+    .status-container{
+        max-width: fit-content;
+    }
     .card-img-top{
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
         max-height: 8rem;
+    }
+
+    .btn-action{
+        width: 2rem;
+        height: 2rem;
+    }
+
+    .btn-visit{
+        height: 2rem;
     }
 </style>
--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -14,6 +14,28 @@
                         description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat, felis euismod consectetur iaculis, nisl purus facilisis urna, nec posuere eros lacus in erat.',
                         images:'./src/assets/images/sample_hero.png',
                         official_link: '',
+                        status: '',
+                        platforms: [
+                        {
+                            type: 'windows',
+                            link: ''
+                        },
+                        {
+                            type: 'apple',
+                            link: ''
+                        },
+                        {
+                            type: 'android',
+                            link: ''
+                        }
+                        ]
+                    },
+                    {
+                        title:'Lucid Melody',
+                        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat, felis euismod consectetur iaculis, nisl purus facilisis urna, nec posuere eros lacus in erat.',
+                        images:'./src/assets/images/sample_hero.png',
+                        official_link: '',
+                        status: 'UNPUBLISHED',
                         platforms: [
                         {
                             type: 'windows',
@@ -34,6 +56,7 @@
                         description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat, felis euismod consectetur iaculis, nisl purus facilisis urna, nec posuere eros lacus in erat.',
                         images:'./src/assets/images/sample_hero.png',
                         official_link: '',
+                        status: 'HIDDEN',
                         platforms: [
                         {
                             type: 'windows',
@@ -73,12 +96,12 @@
                         </span>
                         <input type="text" class="form-control rounded-start-0 border-0 rounded-2 border-start-0 tertiary-font searchbar-grey" placeholder="Search . . " aria-label="Search" >
                     </div>
-                    <router-link to="/admin/add-game"class="route-btn d-flex align-items-center text-decoration-none btn btn-danger rounded-2 mx-0 mx-md-2 rounded-0 px-4 accent-bg accent-border ">
+                    <router-link to="/admin/add-game" class="route-btn d-flex align-items-center text-decoration-none btn btn-danger rounded-2 mx-0 mx-md-2 rounded-0 px-4 accent-bg accent-border ">
                         <i class="bi bi-plus fs-3 secondary-font"></i> <p class="m-0 text-nowrap secondary-font">Add Game</p>
                     </router-link>
                 </div>
             </div>
-            <div class="row w-100 m-auto">
+            <div class="row w-100 m-auto mt-5 px-0">
                 <div v-for="(items, index) in sampleData" :key="index" class="col-xl-4 col-lg-6 m-auto mb-3">
                     <GameCardComponent
                         :data="items"
